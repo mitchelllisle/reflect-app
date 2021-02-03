26cab491bbdb1528a3ad41bd5c726a0c8f1db600
Mitchell Lisle <lislemitchell@gmail.com>
2021-02-03 14:13:25 +0000

    Update run-tests.sh

--- docker/ci/run-tests.sh
+++ docker/ci/run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-sudo pip install --user -r requirements/test.txt
+sudo pip install --user -r requirements/tests.txt
 sudo pip install --user .
 
 sudo python -m pytest --headless --cov=reflect
