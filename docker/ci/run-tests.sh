c1e18e368a686759fba1730db2369a77885f0cb9
Mitchell Lisle <lislemitchell@gmail.com>
2021-02-03 14:16:13 +0000

    tests

--- docker/ci/run-tests.sh
+++ docker/ci/run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-sudo pip install --user -r requirements/tests.txt
-sudo pip install --user .
+sudo pip3 install --user -r requirements/tests.txt
+sudo pip3 install --user .
 
-sudo python -m pytest --headless --cov=reflect
+sudo python3 -m pytest --headless --cov=reflect
